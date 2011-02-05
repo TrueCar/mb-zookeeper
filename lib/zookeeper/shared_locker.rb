cf1038749c08a715b148e14f92097fe2b762036d
slyphon <simms@hp.com>
2011-02-05 06:16:56 +0000

    add note

--- lib/zookeeper/shared_locker.rb
+++ lib/zookeeper/shared_locker.rb
@@ -1,10 +1,13 @@
 module ZooKeeper
   # NOTE: these locks exist in a different namespace than those created w/
   # Locker, and are therefore incompatible
+  #---
+  # XXX: Should probably split this into two classes, ReadLocker and
+  # WriteLocker as the protocols are similar but slightly different
+  #
   class SharedLocker < LockerBase
     READ_LOCK_PREFIX  = 'read'.freeze
     WRITE_LOCK_PREFIX = 'write'.freeze
-
     
     class NoWriteLockFoundException < StandardError #:nodoc:
     end
